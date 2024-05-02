28a41e715ef2e55f4e6b1c7db93434976d2b7ffe
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-03 00:08:39 +0300

    error handling

--- ArtToken.sol
+++ ArtToken.sol
@@ -21,6 +21,9 @@ contract DigitalArtNFT is ERC721URIStorage, Ownable {
         public onlyOwner
         returns (uint256)
     {
+        require(recipient != address(0), "Recipient address cannot be zero.");
+        require(bytes(metadataURI).length > 0, "MetadataURI cannot be empty.");
+
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
@@ -33,24 +36,31 @@ contract DigitalArtNFT is ERC721URIStorage, Ownable {
 
     function transferNFT(address from, address to, uint256 tokenId) public {
         require(_isApprovedOrOwner(_msgSender(), tokenId), "Caller is not owner nor approved");
+        require(to != address(0), "Transfer to the zero address");
+
         _transfer(from, to, tokenId);
         _cachedOwners[tokenId] = to; // Update cached owner upon transfer
     }
 
     function burnNFT(uint256 tokenId) public {
+        require(_exists(tokenId), "Token does not exist.");
         require(_isApprovedOrOwner(_msgSender(), tokenId) || owner() == _msgSender(), "Caller is not owner nor approved or contract owner");
+        
         _burn(tokenId);
         delete _cachedOwners[tokenId]; // Remove cached ownership upon burning
         emit NFTBurned(tokenId);
     }
 
     function updateMetadataURI(uint256 tokenId, string memory newMetadataURI) public {
+        require(_exists(tokenId), "Token does not exist.");
         require(_isApprovedOrOwner(_msgSender(), tokenId), "Caller is not owner nor approved");
+        require(bytes(newMetadataURI).length > 0, "New metadata URI cannot be empty.");
+        
         _setTokenURI(tokenId, newMetadataURI);
         emit MetadataURIUpdated(tokenId, newMetadataURI);
     }
 
-    function queryOwnership(uint256 tokenId) public view returns (address owner) {
+    function queryOwnership(uint256 tokenId) public view returns (address) {
         require(_exists(tokenId), "Query for nonexistent token");
         return _cachedOwners[tokenId]; // Return cached owner
     }
