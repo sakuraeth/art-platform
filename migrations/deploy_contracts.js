6edc9b1bdc2d49ab43244bcdb97fd7a1184ef5ca
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-05 14:01:25 +0300

    implemented new feature

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -5,8 +5,16 @@ const AuctionAccessControl = artifacts.require("AuctionAccessControl");
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(ArtToken);
   const artTokenInstance = await ArtToken.deployed();
+
   await deployer.deploy(AuctionAccessControl);
   const auctionAccessControlInstance = await AuctionAccessControl.deployed();
+
   await deployer.deploy(ArtAuction, artTokenInstance.address, auctionAccessControlInstance.address);
   const artAuctionInstance = await ArtAuction.deployed();
+  
+  if (network !== 'live') {
+    console.log('Setting up demo environment...');
+    await artAuctionInstance.setupDemoEnvironment({ from: accounts[0] });
+    console.log('Demo environment setup complete.');
+  }
 };
