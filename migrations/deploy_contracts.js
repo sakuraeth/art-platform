762ceff7c370ce6765a3677b1c025b4c6c21f107
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-07 05:40:53 +0300

    add new variables

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -1,16 +1,16 @@
-const ArtToken = artifacts.require("ArtToken");
-const ArtAuction = artifacts.require("ArtAuction");
-const AuctionAccessControl = artifacts.require("AuctionAccessControl");
+const ArtTokenContract = artifacts.require("ArtToken");
+const ArtAuctionContract = artifacts.require("ArtAuction");
+const AuctionAccessControlContract = artifacts.require("AuctionAccessControl");
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(ArtToken);
-  const artTokenInstance = await ArtToken.deployed();
+  await deployer.deploy(ArtTokenContract);
+  const artTokenInstance = await ArtTokenContract.deployed();
 
-  await deployer.deploy(AuctionAccessControl);
-  const auctionAccessControlInstance = await AuctionAccessControl.deployed();
+  await deployer.deploy(AuctionAccessControlContract);
+  const accessControlInstance = await AuctionAccessControlContract.deployed();
 
-  await deployer.deploy(ArtAuction, artTokenInstance.address, auctionAccessControlInstance.address);
-  const artAuctionInstance = await ArtAuction.deployed();
+  await deployer.deploy(ArtAuctionContract, artTokenInstance.address, accessControlInstance.address);
+  const artAuctionInstance = await ArtAuctionContract.deployed();
   
   if (network !== 'live') {
     console.log('Setting up demo environment...');
