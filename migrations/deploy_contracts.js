274d57b9eb8d6ba8407caa39e6dbb999356518f3
sakuraeth <163349116+sakuraeth@users.noreply.github.com>
2024-05-09 18:09:07 +0300

    cleaning

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -4,21 +4,36 @@ const AuctionAccessControlContract = artifacts.require("AuctionAccessControl");
 
 module.exports = async function (deployer, network, accounts) {
   try {
-    await deployer.deploy(ArtTokenContract);
-    const artTokenInstance = await ArtTokenContract.deployed();
+    const artTokenInstance = await deployArtToken(deployer);
+    const accessControlInstance = await deployAccessControl(deployer);
+    await deployArtAuction(deployer, artTokenInstance, accessControlInstance);
 
-    await deployer.deploy(AuctionAccessControlContract);
-    const accessControlInstance = await AuctionAccessControlContract.deployed();
-
-    await deployer.deploy(ArtAuctionContract, artTokenInstance.address, accessControlInstance.address);
-    const artAuctionInstance = await ArtAuctionContract.deployed();
-    
     if (network !== 'live') {
-      console.log('Setting up demo environment...');
-      await artAuctionInstance.setupDemoEnvironment({ from: accounts[0] });
-      console.log('Demo environment setup complete.');
+      await setupDemoEnvironment(artTokenInstance, accessControlInstance, accounts);
     }
   } catch (error) {
     console.error("An error occurred during the deployment process:", error);
   }
 };
+
+async function deployArtToken(deployer) {
+  await deployer.deploy(ArtTokenContract);
+  return await ArtTokenContract.deployed();
+}
+
+async function deployAccessControl(deployer) {
+  await deployer.deploy(AuctionAccessControlContract);
+  return await AuctionAccessControlContract.deployed();
+}
+
+async function deployArtAuction(deployer, artTokenInstance, accessControlInstance) {
+  await deployer.deploy(ArtAuctionContract, artTokenInstance.address, accessControlInstance.address);
+  return await ArtAuctionContract.deployed();
+}
+
+async function setupDemoEnvironment(artTokenInstance, accessControlInstance, accounts) {
+  console.log('Setting up demo environment...');
+  const artAuctionInstance = await ArtAuctionContract.deployed();
+  await artAuctionInstance.setupDemoEnvironment({ from: accounts[0] });
+  console.log('Demo environment setup complete.');
+}
